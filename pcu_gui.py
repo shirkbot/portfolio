00696a95e9cc6973f31898e2eb083a466394c0e5
shirkbot <robcthomp@hotmail.com>
2025-11-08 17:11:33 +0000

    Add files via upload
    A small tool for mass deployment of project changes to active projects with a crude GUI

---
+++ pcu_gui.py
@@ -0,0 +1,317 @@
+import tkinter as tin
+from tkinter import ttk
+
+from pipeline_config_utilities import PipelineConfigUtils as pcutils
+
+util_instance = pcutils()
+
+class UtilsWindow():
+
+    # Dict of all config names and their checkboxs
+    values = {}
+
+    def __init__(self, root):
+        # Main Window
+        root.title("Pipeline Config Utils")
+
+        # frame for controlling window size
+        frm_master = tin.Frame(root)
+
+        # Decorative label so people know what they're looking at
+        label = tin.Label(frm_master, text="Pipeline Config Utils", font=('Calibiri', 18))
+        label.grid(column=0, row=0)
+
+        # Frame for the list of configs
+        config_list = tin.LabelFrame(frm_master, text="Configs")
+        config_list.grid(column=0, row=1)
+
+        # de/select all buttons
+        frm_select_all = tin.Frame(config_list)
+        frm_select_all.pack(fill='x')
+
+        btn_select_all = tin.Button(frm_select_all, text='Select All', command=self.select_all)
+        btn_deselect_all = tin.Button(frm_select_all, text='Deselect All', command=self.deselect_all)
+
+        btn_deselect_all.grid(row=0, column=0, sticky='e')
+        btn_select_all.grid(row=0, column=1, sticky='e')
+
+        # order the config list alphabetically
+        config_names = sorted(util_instance.dev_paths_dict.keys())
+
+        # Frames containing the config name and a checkbox
+        for config_name in config_names:
+            config_frame = tin.Frame(config_list)
+
+            config_label = tin.Label(config_frame, text=config_name)
+            config_label.pack(side='left', padx=5)
+
+            val = tin.IntVar(value=1)
+            checkbox = tin.Checkbutton(config_frame, variable=val)
+            checkbox.pack(side='right', padx=5)
+
+            config_frame.pack(fill="x")
+            self.values.update({config_name: val})
+
+        # Frame for holding all the buttons
+        button_frame = tin.Frame(frm_master)
+        button_frame.grid(column=1, row=1, padx=10)
+
+        # buttons
+        btn_single_config_update = tin.Button(
+            button_frame, 
+            text="Update Config(s) from Git", 
+            pady=10, 
+            command=self.single_config_update)
+
+        btn_initial_config_corrections = tin.Button(
+            button_frame, 
+            text="Correct Template References", 
+            pady=10, 
+            command=self.initial_config_corrections)
+
+        btn_replace_templates = tin.Button(
+            button_frame, 
+            text="Remove Git-conflicting Templates", 
+            pady=10, 
+            command=self.replace_templates)
+
+        btn_run_cache_apps = tin.Button(
+            button_frame, 
+            text="Run tank cache_apps", 
+            pady=10, 
+            command=self.run_cache_apps)
+
+        btn_run_push_config = tin.Button(
+            button_frame, 
+            text="Open cmd Windows for Projects", 
+            pady=10, 
+            command=self.run_push_config)
+
+        #TODO Work out a proper method for generating Show Tools
+        btn_generate_showtools = tin.Button(
+            button_frame, 
+            text="Generate Showtools", 
+            pady=10, 
+            command=self.generate_showtools)
+
+        # pack all the buttons
+        [
+            i.pack(fill='x', pady=5) for i in [
+                btn_single_config_update,
+                btn_initial_config_corrections,
+                btn_replace_templates,
+                btn_run_cache_apps,
+                btn_run_push_config,
+                btn_generate_showtools,
+                ]
+        ]
+
+        # pack the frame and display the window
+        frm_master.pack(padx=10,pady=10)
+
+    # button methods
+    # All buttons check for a list of configs, and then require confirmation
+    def single_config_update(self):
+        print('single_config_update was pressed')
+        configs = self.get_checks()
+        user_confirm = self.molly_guard()
+        if not user_confirm:
+            return
+        util_instance.bulk_processing('single_config_update', configs)
+
+    def initial_config_corrections(self):
+        print('initial_config_corrections was pressed')
+        configs = self.get_checks()
+        user_confirm = self.molly_guard()
+        if not user_confirm:
+            return
+        util_instance.bulk_processing('initial_config_corrections', configs)
+
+    def replace_templates(self):
+        print('replace_templates was pressed')
+        template_list = self.select_templates()
+        configs = self.get_checks()
+        user_confirm = self.molly_guard()
+        if not user_confirm:
+            return
+        util_instance.bulk_processing('replace_templates', configs, template_list)
+
+    def run_cache_apps(self):
+        print('run_cache_apps was pressed')
+        configs = self.get_checks()
+        user_confirm = self.molly_guard()
+        if not user_confirm:
+            return
+        util_instance.bulk_processing('run_cache_apps', configs)
+
+    def run_push_config(self):
+        print('run_push_config was pressed')
+        configs = self.get_checks()
+        user_confirm = self.molly_guard()
+        if not user_confirm:
+            return
+        util_instance.bulk_processing('run_push_config', configs)
+        
+    def generate_showtools(self):
+        print('generate_showtools was pressed')
+        configs = self.get_checks()
+        if len(configs) > 1:
+            print("Select a single config to continue")
+            return
+            
+        user_confirm = self.molly_guard()
+        if not user_confirm:
+            return
+
+        config = configs[0]
+        util_instance.generate_showtools(config)
+
+
+    # utility methods
+    def get_checks(self):
+        """
+        Generate a list of all configs with checkboxes marked
+        """
+        print('Generating list of configs to process...')
+        processing_list = [config for config, check in self.values.items() if check.get() != 0]
+        print(f'returning configs {processing_list}')
+        return processing_list
+
+    def select_all(self):
+        """
+        Mark all checkboxes
+        """
+        [check.set(1) for config,check in self.values.items()]
+
+    def deselect_all(self):
+        """
+        Clear all checkboxes
+        """
+        [check.set(0) for config,check in self.values.items()]
+
+    def select_templates(self):
+        """
+        Generate a popup to confirm which templates the user wishes to remove
+        """
+        template_list = TemplateWindow().template_list
+        return template_list
+
+    def molly_guard(self):
+        """
+        Generate a popup to get user confirmation that their config list is final
+
+        NOTE: Main window will be inactive until confirmed or closed
+        """
+        user_confirm = MollyGuard().confirm
+        if not user_confirm:
+            print("User has not confirmed config selection. Aborting...")
+
+        return user_confirm
+
+class TemplateWindow():
+    
+    template_list = []
+    # TODO: make a dynamic list of templates
+    available_templates = [
+        "roots"
+        # other templates removed for NDA reasons
+    ]
+
+    def __init__(self):
+        # Popout Window
+        template_window = tin.Toplevel()
+        template_window.title("Select Templates")
+
+        # master frame for Template window
+        template_master_frm = tin.Frame(template_window)
+
+        # Label to identify popout window
+        template_lbls = tin.Label(template_master_frm, text="Select Templates to Replace", font=('Calibiri', 18), padx=10)
+        template_lbls.pack(fill='x')
+
+        # Frame to contain list of templates and checkmarks
+        template_lblfrm = tin.LabelFrame(template_master_frm, text='Templates')
+        template_lblfrm.pack()
+
+        # generate frames with template names and checkmarks
+        template_values = {}
+        for template in self.available_templates:
+            template_frm = tin.Frame(template_lblfrm)
+
+            template_lbl = tin.Label(template_frm, text=template)
+            template_lbl.pack(side='left', padx=5)
+
+            val = tin.IntVar()
+            template_cbx = tin.Checkbutton(template_frm, variable=val)
+            template_cbx.pack(side='right', padx=5)
+
+            template_frm.pack(fill="x")
+            template_values.update({template: val})
+
+        # confirmation button
+        template_confirm_btn = tin.Button(
+            template_master_frm, 
+            text="Confirm", 
+            pady=10, 
+            command=template_window.destroy)
+        template_confirm_btn.pack(side='right', padx=50, pady=10)
+
+        # Set window dimensions and render
+        template_master_frm.pack(padx=10,pady=10)
+
+        template_window.wait_window()
+
+        # collect list of templates to remove
+        self.template_list = [template for template,check in template_values.items() if check.get() != 0]
+        print(f"returning templates: {self.template_list}")
+
+class MollyGuard():
+    
+    confirm = False
+
+    def __init__(self):
+        # Popout Window
+        guard_window = self.guard_window = tin.Toplevel()
+        guard_window.title("Confirm Config Selection")
+
+        # label to identify the window
+        guard_lbl = tin.Label(guard_window, text="Confirm Config Selection", font=('Calibiri', 18), padx=10)
+        guard_lbl.pack(fill='x')
+
+        # confirmation button
+        user_confirm_btn = tin.Button(
+            guard_window, 
+            text="Confirm", 
+            padx=30, 
+            pady=30, 
+            font=('Calibiri', 24),
+            bg="Green",
+            command=self.user_confirm)
+        user_confirm_btn.pack(padx=50, pady=10)
+
+        guard_window.update()
+        # window height,width
+        width = guard_lbl.winfo_width()
+        height = (guard_lbl.winfo_height() + user_confirm_btn.winfo_height())+50
+
+        # window x,y
+        posx = int((guard_window.winfo_screenwidth()/2) - (width/2))
+        posy = int((guard_window.winfo_screenheight()/2) - (height/2))
+
+        # final geometry
+        guard_window.geometry(f"{width}x{height}+{posx}+{posy}")
+
+        # disable main window while dialog is active
+        guard_window.grab_set()
+        guard_window.wait_window()
+
+    def user_confirm(self):
+        """
+        Set confirm value to TRUE for evaluation by main window and destroy dialog
+        """
+        self.confirm = True
+        self.guard_window.destroy()
+
+root = tin.Tk()
+window = UtilsWindow(root)
+root.mainloop()
---
+++ pipeline_config_utilities.py
@@ -0,0 +1,337 @@
+import getpass
+import os
+import re
+import shutil
+import subprocess
+
+from datetime import datetime
+
+class PipelineConfigUtils():
+
+    # core variables
+    dev_paths_dict = {}
+    sg_returns = None
+    
+    def __init__(self):
+        # Instance SG Engine
+        try:
+            # Instance Shotgun Engine from sg_tools
+            from sgpy.sg_tools import sg_connection
+            eng = sg_connection.sg_connect()
+            print("SG Engine from sg_tools")
+        except:
+            # Instance Shotgun Engine from local API files
+            print("Couldn't find sg_connection module, looking for local sgtk")
+            import shotgun_api3
+            eng = shotgun_api3.shotgun.Shotgun(
+                # Args removed for NDA coverage
+                )
+            print("SG Engine from local API")
+
+        # Query SG for config information
+        # Set Shotgun search filters/fields
+        filters = [
+            # Args removed for NDA coverage
+            ]
+        fields = # Args removed for NDA coverage
+
+        # Run Shotgun search
+        # returns all projects marked "Active"
+        self.sg_returns = eng.find("PipelineConfiguration", filters, fields)
+
+        # create mapped drive path with string replacement variable
+        # this is used in place of os.path.join because path.join creates literal \\ character
+        mapped_drives = {
+            # Args removed for NDA coverage
+        }
+
+        for config in self.sg_returns:
+            network_path = config.get('windows_path')
+            root_name = # Args removed for NDA coverage
+
+            # remove anything that doesn't have a Windows path
+            if not network_path:
+                print("%s missing windows path" % root_name)
+                continue
+
+
+            # remove Pipeline configs
+            if root_name in [
+                # Args removed for NDA coverage
+                ]:
+                continue
+
+            storage_name = config['project.Project']['local_storage']['name']
+
+            path_split = [i for i in network_path.split('\\') if i]
+            drive_map = [mapped_drives.get(path_split[0])]
+            drive_map.extend(path_split[2:])
+            mapped_path = "\\".join(drive_map)
+            
+            self.dev_paths_dict.update({
+                drive_map[-1]: {
+                    # Args removed for NDA coverage
+                }
+            })
+            print("%s:  %s" % (
+                storage_name,
+                path_split
+            ))
+
+    def set_path_dict(self, current_dir):
+        """
+        create a set of paths that includes all .yml files in need of replacements
+
+        :current_dir: root path
+        """
+        # Sets necessary paths to loop through for editing
+        git_dir = os.path.join(current_dir, 'config')
+        core_dir = os.path.join(git_dir, 'core')
+        includes_dir = os.path.join(core_dir, 'includes')
+        schema_dir = os.path.join(core_dir, 'schema')
+
+        dir_dictionary = {
+            "core": {
+                "path": core_dir,
+                "files": ['roots', 'templates', 'install_location']
+            },
+            "includes": {
+                "path": includes_dir,
+                "files": [os.path.splitext(template_file)[0] for template_file in os.listdir(includes_dir) if os.path.splitext(template_file)[-1] != ".old"]
+            },
+            "schema": {
+                "path": schema_dir,
+                "files": ['project']
+            },
+        }
+
+        return dir_dictionary
+
+    def generate_showtools(self, storage_root_name):
+        """
+        generate a show folder in [redacted]
+
+        :param: storage_root_name - name of the storage root in SG  
+        """
+        # isolate the root
+        root = next((i for i in self.sg_returns if str(i.get('windows_path')).endswith(storage_root_name)), None)
+        
+        # capture name to assign directory
+        root_name = root.get('project.Project', {}).get('name')
+
+        showtools_redacted2 = "[redacted]"
+        showtools_redacted = "[redacted]"
+
+        # TODO Migrate ShowTools to Pix
+        showtools_root = showtools_redacted2
+        if os.path.exists(showtools_redacted):
+            showtools_root = showtools_redacted
+
+        # Configure paths to copy and destination
+        template_name = "_TEMPLATE"
+        showtools_template = os.path.normpath(os.path.join(showtools_root, template_name))
+        showtools_dir = re.sub(template_name, root_name, showtools_template)
+
+        if not os.path.exists(showtools_dir):
+            print(f"Generating show_tools directory for: {showtools_dir}")
+            shutil.copytree(showtools_template, showtools_dir)
+
+    def single_config_update(self, config):
+        """
+        Creates/clones a config folder with a git directory
+        and creates a backup of the previous config folder in case of emergency
+
+        :config: Specific config folder name in Configs directory
+        """
+        # Set current directory
+        dev_path_info = self.dev_paths_dict.get(config)
+        current_dir = dev_path_info.get('mapped_path')
+
+        # Get the name of the current user
+        user = getpass.getuser()
+        time = datetime.now()
+        timestamp = time.strftime('%Y%m%d_%H%M%S')
+
+        # config root of dir\\config and backup directory
+        config_root = os.path.join(current_dir, 'config')
+        backup_dir = os.path.join(current_dir, 'config.bak', 'config.{timestamp}.{user}'.format(timestamp=timestamp, user=user))
+
+        # create a cmd string using %s or fstring because .format displays as an extension of the web address
+        cmd_string = f"cd /d {config_root} & git pull [redacted]"
+
+        # Create a backup of the current config folder and pull latest config settings from Github
+        try:
+            print('Creating config backup')
+            shutil.copytree(config_root, backup_dir)
+
+            print(f'Attempting git pull for {config}')
+            subprocess.call(cmd_string, shell=True)
+
+        # Create a fresh config directory and pull the latest config settings from Github
+        except:
+            print('No config, Creating new git directory')
+            os.makedirs(config_root)
+
+            print('Creating config backup')
+            subprocess.call(f"cd /d {config_root} & git init", shell=True)
+
+            print(f'Attempting git pull for {config}')
+            subprocess.call(cmd_string, shell=True)
+
+    def initial_config_corrections(self, config):
+        """
+        Replaces all references to Pipeline within key yml files
+        """
+
+        # Sets current directory path and retrieves the project root name
+        dev_path_info = self.dev_paths_dict.get(config)
+        current_dir = dev_path_info.get('mapped_path')
+        project_root = dev_path_info.get('redacted_storage')
+
+        # Sets necessary paths to loop through for editing
+        dir_dictionary = self.set_path_dict(current_dir)
+        
+        print(f"processing {config}...")
+        for location, info in dir_dictionary.items():
+            for file_name in info['files']:
+                dir_file = os.path.join( info['path'], file_name )
+                clone_doc = open( f'{dir_file}_clone.txt', 'a' )
+
+                try:
+                    # convert yml file to txt for easier editing
+                    # (because I don't want to import more modules)
+                    os.rename( f'{dir_file}.yml', f'{dir_file}.txt' )
+                    temp_file = open( f'{dir_file}.txt', 'r' )
+                    read_of_file = temp_file.read()
+                    temp_file.close()
+
+                    # Check for existing .old file and overwrite it, or create a new one
+                    try:
+                        doc_check = open( f'{dir_file}.yml.old' )
+                        doc_check.close()
+                        os.remove( f'{dir_file}.yml.old' )
+                        os.rename( f'{dir_file}.txt', f'{dir_file}.yml.old' )
+                    except:
+                        os.rename( f'{dir_file}.txt', f'{dir_file}.yml.old' )
+
+                    count = 0
+
+                    # The actual replacements happen here
+                    for read_line in read_of_file.split( '\n' ):
+                        if 'windows_path' in read_line or 'linux_path' in read_line:
+                            clone_doc.write( read_line.replace( 'project_pipeline', project_root.replace('[redacted]','') ) + '\n' )
+                            count +=1
+                        elif 'project_pipeline' in read_line:
+                            clone_doc.write( read_line.replace( 'project_pipeline', project_root ) + '\n' )
+                            count +=1
+                        else:
+                            clone_doc.write( read_line + '\n' )
+
+                    print( f'{str(count)} instances of "project_pipeline" in {dir_file}.yml' )
+
+                except:
+                    print( 'No such file' )
+
+                # Close document and convert it to a .yml so it can be read correctly by Shotgun
+                clone_doc.close()
+                os.rename( f'{dir_file}_clone.txt', f'{dir_file}.yml' )
+
+    def replace_templates(self, config, template_list = []):
+        """
+        renames an existing template to template.old across all active projects.
+        This is intended to be used when doing bulk config updates to clear a common git pull conflict
+
+        :template_to_replace: name of the template.yml file that needs to be removed. If not specified
+        """
+
+        dev_path_info = self.dev_paths_dict.get(config)
+        current_dir = dev_path_info.get('mapped_path')
+        print("Current Directory is " + current_dir)
+        
+        # Sets necessary paths to loop through for editing
+        dir_dictionary = self.set_path_dict(current_dir)
+
+        # add templates.yml and roots.yml to list of template paths
+        available_paths = [os.path.join(dir_dictionary['core']['path'], 'roots')]
+        available_paths.extend([os.path.join(dir_dictionary['includes']['path'], i) for i in dir_dictionary['includes']['files']])
+
+        if not template_list:
+            select_paths = [f"{path}.yml" for path in available_paths]
+        elif "templates" in template_list:
+            minus_list = [i for i in template_list if i != 'templates']
+            template_path = f"{os.path.join(dir_dictionary['core']['path'], 'templates')}.yml"
+            select_paths = [f"{path}.yml" for path in available_paths if path.endswith(tuple(minus_list))]
+            select_paths.append(template_path)
+        else:
+            select_paths = [f"{path}.yml" for path in available_paths if path.endswith(tuple(template_list))]
+
+        for item in select_paths:
+            try:
+                doc_check = open(item + '.old')
+                doc_check.close()
+                os.remove(item + '.old')
+                os.rename(item, item + '.old')
+            except:
+                os.rename(item, item + '.old')
+
+        print(f"The following templates have been removed from {config}: {select_paths}")
+
+    def run_cache_apps(self, config):
+        """
+        Opens a cmd window in the specified project config
+
+        :config: Specific config folder name in Configs directory
+        """
+        dev_path_info = self.dev_paths_dict.get(config)
+        current_dir = dev_path_info.get('mapped_path')
+
+        print(f"Caching apps for: {config}")
+
+        subprocess.run(["tank", "cache_apps"], shell=True, cwd=current_dir)
+
+    def run_push_config(self, config):
+        """
+        Opens a cmd window in the specified project config
+
+        :config: Specific config folder name in Configs directory
+        """
+        dev_path_info = self.dev_paths_dict.get(config)
+        current_dir = dev_path_info.get('mapped_path')
+
+        subprocess.Popen(["start"], shell=True, cwd=current_dir)
+
+
+    def bulk_processing(self, process, config_list = [], template_list = []):
+        """
+        Allows for the processing of multiple configs
+        but only ONE process at a time, for cleanliness
+
+        :process: name of the process to be run (config_update, config_correction, replace_templates)
+        :config_list: list of configs to be processed.
+            If not specified runs on all Pipeline Active projects
+        :template_list: name of the template .yml file(s) that needs to be removed.
+            If not specified clears all template yml files
+        """
+        # decide what configs to process
+        # if no configs are specified, process all Pipeline Active configs
+        if not config_list:
+            config_list = self.dev_paths_dict.keys()
+
+        # decide how you want to process them
+        if process == "single_config_update":
+            for config in config_list:
+                self.single_config_update(config)
+        elif process == "initial_config_corrections":
+            for config in config_list:
+                self.initial_config_corrections(config)
+        elif process == "replace_templates":
+            for config in config_list:
+                self.replace_templates(config, template_list)
+        elif process == "run_cache_apps":
+            for config in config_list:
+                self.run_cache_apps(config)
+        elif process == "run_push_config":
+            for config in config_list:
+                self.run_push_config(config)
+
+        print(f"END OF BULK {process}")
